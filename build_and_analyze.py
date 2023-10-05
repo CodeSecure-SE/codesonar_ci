e8303e11bebff22b1d41ce2de16464c85062aa7a
Mark Hermeling <mark@hermeling.org>
2023-10-05 12:52:49 -0400

    Feature/add gitlab (#1)
    * Make script more generic
    
    * Add a sleep betweeb build and analyze
    
    * Add shutdown of build process connection
    
    * Fix error on 118
    
    * Debugging
    
    * Damn, wrong aid
    
    * Wrong spot
    
    * debugging
    
    * Tetsing
    
    * ignore exception during close of the connection
    
    * fix syntax error
    
    * fix indent error
    
    * Typo
    
    * mods
    
    * Add commit link property
    
    * Fix the close URL
    
    * Try with subprocess
    
    * Build
    
    * LIsts
    
    * list
    
    * Remove debugging code
    
    * Fix check for exit_code
    
    * Add branch_name property
    
    * Close the build
    
    * change to commandstr
    
    * $TARGET -> TARGET
    
    * Fixes
    
    * Update name
    
    * Close the build
    
    ---------
    
    Co-authored-by: Mark Hermeling <mhermeling@codesecure.com>

--- build_and_analyze.py
+++ build_and_analyze.py
@@ -6,10 +6,13 @@
 
 
 import os
+import time
 import subprocess
 import sys
 import urllib.parse
 from datetime import datetime
+import urllib.request, urllib.error
+import subprocess
 
 
 debug = True
@@ -21,9 +24,10 @@ if (len(sys.argv) < 2):
     print("Usage: build_and_analyze.py <conf-file> <build-command>")
     sys.exit(1)
 conf_file = sys.argv[1]
-build_command = " ".join(sys.argv[2:])
+build_command=[]
+build_command = sys.argv[2:]
 
-print ("Build command: " + build_command)
+
 all_ok=1
 
 def check_env(s, t):
@@ -40,16 +44,17 @@ check_env('CSONAR_HUB_PASSWORD', 'Password for CodeSonar HUB')
 check_env('CSONAR_CSHOME', 'Path to CodeSonar installation')
 check_env('ROOT_TREE', 'Path to the project-tree in the CodeSonar HUB')
 check_env('PROJECT_NAME', 'Name of the project in the CodeSonar HUB')
-check_env('GITHUB_API_URL', 'URL for GitHub API')
-check_env('GITHUB_TOKEN', 'Token for GitHub API')
-check_env('GITHUB_CAFILE', 'Path to GitHub CA (cert) file')
-check_env('GITHUB_REPO_URL', 'URL for GitHub repository')
-check_env('PULL_REQUEST_NUMBER', 'Pull request ID from ${{ github.event.pull_request.number }}')
-check_env('BRANCH_NAME', 'Name of the current branch, ${{ github.head_ref || github.ref_name }} ')
-check_env('IS_PR', 'Set to true if this is a pull request, ${{ github.event_name == \'pull_request\' }}')
-check_env('TARGET', 'Target branch for the pull request, ${{ github.base_ref || github.target_branch }}')
+check_env('TOKEN', 'Token API')
+check_env('CAFILE', 'Path to CA (cert) file')
+check_env('REPO_URL', 'URL for repository')
+check_env('REQUEST_NUMBER', 'Pull/Merge request ID')
+check_env('BRANCH_NAME', 'Name of the current branch ')
+check_env('IS_PR', 'Set to true if this is a pull/merge request')
+check_env('TARGET', 'Target branch for the pull/merge request')
+check_env("COMMIT_HASH", "Commit hash")
 
-#TODO: Add GITLAB equivalents
+
+#TODO: Change PR to Request Number
 
 
 if all_ok==0:
@@ -67,7 +72,7 @@ target_project_aid = 0
 # If this is a PR/MR, find the analysis-id of the latest analysis on the target branch
 if os.getenv('IS_PR') == 'pull_request':
     link = "{\"limit\":1,\"orderBy\":[{\"analysisId\":\"DESCENDING\"}],\"columns\":[\"analysisId\"]}"
-    query = "\"branch.name\"=\"" + os.getenv("TARGET") + "\"state=\"Finished\""
+    query = "\"branch_name\"=\"" + os.getenv("TARGET") + "\"state=\"Finished\""
     
     command = os.getenv('CSONAR_CSHOME') + "/codesonar/bin/codesonar get -auth password -hubuser " + \
         os.getenv('CSONAR_HUB_USER') + " -hubpwfile " + CSONAR_HUB_PW_FILE + " " + \
@@ -94,23 +99,41 @@ if os.getenv('IS_PR') == 'pull_request':
     f.close()
  
     if debug:
-        print ("Target project analysis id: " + target_project_aid)
+        print ("Target project analysis id: " + str(target_project_aid))
+namestr = datetime.now().strftime("%m/%d/%Y-%H:%M:%S")
 
 #Perform the actual build
-command = os.getenv('CSONAR_CSHOME') + "/codesonar/bin/codesonar build -clean " + \
-os.getenv("PROJECT_NAME") + " -remote-archive \"/saas/*\" -foreground " + \
-            " -auth password" + \
-            " -hubuser " + os.getenv('CSONAR_HUB_USER') + " -hubpwfile " + CSONAR_HUB_PW_FILE + \
-            " -project " + os.getenv("ROOT_TREE") + "/" + os.getenv("BRANCH_NAME") + \
-            " -name \"" + datetime.now().strftime("%m/%d/%Y-%H:%M:%S") +"\"" + \
-            " -conf-file " + conf_file + \
-            " " + os.getenv("CSONAR_HUB_URL") + " " + \
-            build_command
+command = [os.getenv('CSONAR_CSHOME') + "/codesonar/bin/codesonar", 
+                      "build",
+                      "-clean",
+                      os.getenv("PROJECT_NAME"),
+                      "-remote-archive", "/saas/*",
+                        "-foreground",
+                        "-auth", "password", 
+                        "-hubuser", os.getenv('CSONAR_HUB_USER'),
+                        "-hubpwfile", CSONAR_HUB_PW_FILE,
+                        "-project", os.getenv("ROOT_TREE") + "/" + os.getenv("BRANCH_NAME"),
+                        "-name", namestr,
+                        "-conf-file", conf_file,
+                        os.getenv("CSONAR_HUB_URL")] + build_command
+
+p = subprocess.Popen(command, shell=False)
+result = p.wait()
+    
+#command = os.getenv('CSONAR_CSHOME') + "/codesonar/bin/codesonar build -clean " + \
+#os.getenv("PROJECT_NAME") + " -remote-archive \"/saas/*\" -foreground " + \
+#           " -auth password" + \
+#            " -hubuser " + os.getenv('CSONAR_HUB_USER') + " -hubpwfile " + CSONAR_HUB_PW_FILE + \
+#            " -project " + os.getenv("ROOT_TREE") + "/" + os.getenv("BRANCH_NAME") + \
+#            " -name \"" + datetime.now().strftime("%m/%d/%Y-%H:%M:%S") +"\"" + \
+#            " -conf-file " + conf_file + \
+#            " " + os.getenv("CSONAR_HUB_URL") + " " + \
+#            build_command
+
 
 if debug:
     print(command)
 
-result = os.system(command) 
 
 if result != 0:
     print ("Problem running build command")
@@ -118,7 +141,7 @@ if result != 0:
 
 #Construct the properties
 if os.getenv('IS_PR') == 'pull_request':
-    property_pr_link = os.getenv('GITHUB_REPO_URL') + "/pull/" + os.getenv('PULL_REQUEST_NUMBER')
+    property_pr_link = os.getenv('REPO_URL') + "/pull/" + os.getenv('REQUEST_NUMBER')
 else:
     property_pr_link = "Not available"
         
@@ -127,6 +150,7 @@ if debug:
 
 f = open (os.getenv("PROJECT_NAME") + ".prj_files/aid.txt", "r")
 current_project_aid = f.read()
+print(" Current project aid: " + str(current_project_aid))
 f.close()
 
 if os.getenv('IS_PR') == 'pull_request':
@@ -135,53 +159,62 @@ if os.getenv('IS_PR') == 'pull_request':
         "&scope=" + urllib.parse.quote("aid:" + str(current_project_aid)) + "&swarnings=BJAW"
 else:
     property_new_findings = "Not available"
+
+property_commit_link = os.getenv('REPO_URL') + "/commit/" + os.getenv('COMMIT_HASH')
+
+#Make sure the connection is closed
+os.system(os.getenv('CSONAR_CSHOME') + "/codesonar/bin/codesonar get -auth password -hubuser " + \
+        os.getenv('CSONAR_HUB_USER') + " -hubpwfile " + CSONAR_HUB_PW_FILE + " " + \
+        os.getenv('CSONAR_HUB_URL') + "/command/close/" + str(current_project_aid) + "/")
     
 if debug:
     print("New findings: " + property_new_findings)
             
-command = os.getenv('CSONAR_CSHOME') + "/codesonar/bin/codesonar analyze " + \
-            os.getenv("PROJECT_NAME") + " -remote-archive \"/saas/*\" -foreground " + \
+commandstr = os.getenv('CSONAR_CSHOME') + "/codesonar/bin/codesonar analyze " + \
+     os.getenv("PROJECT_NAME") + " -remote-archive \"/saas/*\" -foreground " +  \
             " -auth password" + \
             " -hubuser " + os.getenv('CSONAR_HUB_USER') + " -hubpwfile " + CSONAR_HUB_PW_FILE + \
             " -project " + os.getenv("ROOT_TREE") + "/" + os.getenv("BRANCH_NAME") + \
-            " -property New_findings \"" + property_new_findings + "\""\
-            " -property PR_link \"" +  property_pr_link + "\"" \
-            " -property target.branch \"" + os.getenv("BRANCH_NAME") + "\"" \
-            " -name \"" + datetime.now().strftime("%m/%d/%Y-%H:%M:%S") +"\"" + \
+            " -property New_findings \"" + property_new_findings + "\"" +\
+            " -property PR_link \"" +  property_pr_link + "\"" +\
+            " -property target_branch \"" + os.getenv("TARGET") + "\"" +\
+            " -property commit_link \"" + property_commit_link + "\"" +\
+            " -property branch_name \"" + os.getenv("BRANCH_NAME") + "\"" +\
+            " -name \"" + namestr +"\"" + \
             " -conf-file " + conf_file + \
             " " + os.getenv("CSONAR_HUB_URL") 
 
 if debug:
-    print(command)
+    print(commandstr)
 
-result = os.system(command) 
+result = os.system(commandstr) 
 if result != 0:
     print ("Problem running analyze command")
     sys.exit(1)             
 
 # Download the new findings results in SARIF, if it is a pull request
 if os.getenv('IS_PR') == 'pull_request':
-    command = os.getenv('CSONAR_CSHOME') + "/codesonar/bin/codesonar get -auth password -hubuser " + \
+    commandstr = os.getenv('CSONAR_CSHOME') + "/codesonar/bin/codesonar get -auth password -hubuser " + \
         os.getenv('CSONAR_HUB_USER') + " -hubpwfile " + CSONAR_HUB_PW_FILE + " " + \
         "\"" +os.getenv('CSONAR_HUB_URL') + "/warning_detail_search.sarif?query=" + \
         urllib.parse.quote("aid:"+str(current_project_aid) + " DIFFERENCE aid:" + str(target_project_aid)) + \
         "&scope=" + urllib.parse.quote("aid:" + str(current_project_aid)) + "&swarnings=BJAW\"" + \
         " -o - > warnings.sarif"
     if debug: 
-        print ("Command: " + command)
+        print ("Command: " + commandstr)
         
-    result = os.system(command)
+    result = os.system(commandstr)
     
     if result != 0:
         print ("Error retrieving SARIF file")
         sys.exit(1)
         
     #Convert to summary
-    command = os.getenv("CSONAR_CSHOME")+"/codesonar/bin/cspython " + \
+    commandstr = os.getenv("CSONAR_CSHOME")+"/codesonar/bin/cspython " + \
         "/opt/codesonar-github/sarif_summary.py warnings.sarif " + \
         os.getenv("CSONAR_HUB_URL") + " " + \
         os.getenv("PROJECT_NAME") + " > warnings.md"
-    result=os.system(command)
+    result=os.system(commandstr)
     
     if result != 0:
         print ("Error converting SARIF file to markdown")
