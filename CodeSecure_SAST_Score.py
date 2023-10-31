8f787edf509d766cc618b73125427b89d7eb3da8
Mark Hermeling <mark@hermeling.org>
2023-10-31 07:02:30 -0600

    Added reading of LOC

--- CodeSecure_SAST_Score.py
+++ CodeSecure_SAST_Score.py
@@ -1,11 +1,14 @@
 import csv
+import sys
+import os.path
 
 
 if (len(sys.argv) < 2):
     print("Insufficient parameters, exiting")
-    print("Usage: CodeSecure_SAST_Score.py <csv-file>")
+    print("Usage: CodeSecure_SAST_Score.py aid")
+    print ("   This expects <aid>.csv and <aid>-files.csv to be in the current directory")
     sys.exit(1)
-csv_file = sys.argv[1]
+
 
 
 
@@ -14,10 +17,20 @@ allSignificance = []
 completeFile = []
 
 index = 0
+warningsFile=sys.argv[1]+".csv"
+locFile=sys.argv[1]+"-files.csv"
+
+if (not os.path.isfile(warningsFile)):
+    print ("File not found: "+warningsFile)
+    sys.exit(1)
+
+if (not os.path.isfile(locFile)):
+    print ("File not found: "+locFile)
+    sys.exit(1)
 
 # With code from Alex Hermeling
 
-with open(csv-file,"r") as csv_file:
+with open(warningsFile,"r") as csv_file:
     csv_reader = csv.reader(csv_file)
     for i in csv_reader:
         completeFile.append(i)
@@ -43,4 +56,16 @@ with open(csv-file,"r") as csv_file:
             allTempSig.append(TempSig)
         filePathAndSig.append([filePaths[i], list(allTempSig)])
         allTempSig.clear()
-    print(filePathAndSig)
+
+csv_file.close()
+print("Warnings read from "+warningsFile)     
+locCount = {}
+totalLoc = 0
+with open(locFile,"r") as loc_file:
+    loc_reader = csv.reader(loc_file)
+    next (loc_reader)
+    for row in loc_reader:
+        locCount[row[0]] = int(row[2])
+        totalLoc += int(row[2])
+
+print( "Lines of code (" + str(totalLoc) + ") read from "+locFile)
